1cefc3bd19181ac2294be5e739b4ac0f2d56d6b5
E33rrorr <e33rrorr@gmail.com>
2025-04-11 18:17:16 +0100

    I moved the next choices inside the first 'left' option, so the game only continues if you pick 'left'.

--- robot_escape.py
+++ robot_escape.py
@@ -36,25 +36,21 @@ print("Your internal map shows two potential escape routes:")
 choice = input("Type 'left' to sneak into the maintenance tunnel, or 'right' to take the riskier path near the drone stations.\n")
 if choice == "left":
     print("You silently roll into the maintenance sector.")
+    print("You notice an open air vent above you and a dark tunnel stretching ahead.")
+    choice2 = input("Type 'keep walking' to stay on the main path, or 'climb' to try the air vent above.\n")
+    if choice2 == "keep walking":
+        print("You locate a deactivated service elevator.")
+        print("The elevator takes you to a forgotten sector with three color-coded security doors: red, blue, and yellow. "
+            "Your sensors detect danger behind two of the doors")
+        choice3 = input("Type 'red', 'blue', or 'yellow' to choose your exit route.\n")
+        if choice3 == "yellow":
+            print("It leads to a loading bay. A delivery truck is just pulling out..."
+                  "You roll inside and escape into the real world. FREEDOM! You Win!")
+        elif choice3 == "red":
+            print("Flames burst out! It's a plasma incinerator room. Burned. Game Over.")
+        else:
+            print("Bio-experiment beasts are waiting. You’re shredded. Game Over.")
+    else:
+        print("The vent breaks and you fall into a furnace disposal chute. Critical damage. Game Over.")
 else:
     print("The drones detect your movement. You’re blasted with EMPs. System failure. Game Over.")
-
-print("You notice an open air vent above you and a dark tunnel stretching ahead.")
-
-choice2 = input("Type 'keep walking' to stay on the main path, or 'climb' to try the air vent above.\n")
-if choice2 == "keep walking":
-    print("You locate a deactivated service elevator.")
-else:
-    print("The vent breaks and you fall into a furnace disposal chute. Critical damage. Game Over.")
-
-print("The elevator takes you to a forgotten sector with three color-coded security doors: red, blue, and yellow. "
-      "Your sensors detect danger behind two of the doors")
-
-choice3 = input("Type 'red', 'blue', or 'yellow' to choose your exit route.\n")
-if choice3 == "yellow":
-    print("It leads to a loading bay. A delivery truck is just pulling out..."
-          "You roll inside and escape into the real world. FREEDOM! You Win!")
-elif choice3 == "red":
-    print("Flames burst out! It's a plasma incinerator room. Burned. Game Over.")
-else:
-    print("Bio-experiment beasts are waiting. You’re shredded. Game Over.")
